2c3b70fbce60d460c5ad7eda14158b56dbb19710
Podolskij <Podolskij1994@inbox.ru>
2020-02-06 12:04:49 +0300

    Первая страница без футера

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $('.main-slider').slick({
+        arrows: false,
+        fade: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+});
